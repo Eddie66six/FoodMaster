64b4bbc97c7b5b88e34f057b78168783e1af09ce
guilherme rodrigues souza <guilherme-rsouza@hotmail.com>
2017-04-19 22:03:02 -0300

    alteração de teste

--- src/G2xFoodMaster.Teste.Unitario/Compra/PedidoTeste.cs
+++ src/G2xFoodMaster.Teste.Unitario/Compra/PedidoTeste.cs
@@ -15,40 +15,35 @@ namespace G2xFoodMaster.Teste.Unitario.Compra
     [TestClass]
     public class PedidoTeste
     {
-        private readonly Estabelecimento _estabelecimento;
-        private readonly Filial _filial;
-        private readonly Produto _produto;
-        private readonly Login _login;
-        private readonly Funcionario _funcionario;
-        private readonly Cliente _cliente;
-        private readonly Pedido _pedido;
-        private readonly List<PedidoItem> _pedidoItems;
-        public PedidoTeste()
-        {
-            _estabelecimento = new Estabelecimento("G2x");
-            _filial = new Filial("G2x-filail 1", _estabelecimento);
-            _produto = new Produto("123", "pizza", "seila", 30, "", _filial);
-            _login = new Login("gui", "123");
-            _funcionario = new Funcionario(ETipoFuncionario.Gerente, "guilherme", "rodrigues", "gui", "quintais", "18052601", "emiliano ramos", "399", "casa", "sorocaba sp", new DateTime(1991, 05, 23), _login, _filial);
-            _cliente = new Cliente("guilherme", "rodrigues", "gui", "quintais", "18052601", "emiliano ramos", "399", "casa", "sorocaba sp", new DateTime(1991, 05, 23), _login, _funcionario);
-            _pedido = new Pedido(_cliente);
-            _pedidoItems = new List<PedidoItem>()
-            {
-                new PedidoItem(0,null,_produto),
-                new PedidoItem(0,null,_produto)
-            };
-        }
+
         [TestMethod]
         public void ObterTitulosConcatenados()
         {
-            
-            _pedido.AdicionarItens(_pedidoItems,_funcionario);
+           var estabelecimento = new Estabelecimento("G2x");
+           var filial = new Filial("G2x-filail 1", estabelecimento);
+           var produto = new Produto("123", "pizza", "seila", 30, "", filial);
+           var login = new Login("gui", "123");
+           var funcionario = new Funcionario(ETipoFuncionario.Gerente, "guilherme", "rodrigues", "gui", "quintais", "18052601", "emiliano ramos", "399", "casa", "sorocaba sp", new DateTime(1991, 05, 23), login, filial);
+           var cliente = new Cliente("guilherme", "rodrigues", "gui", "quintais", "18052601", "emiliano ramos", "399", "casa", "sorocaba sp", new DateTime(1991, 05, 23), login, funcionario);
+           var pedido = new Pedido(cliente);
+            var pedidoItems = new List<PedidoItem>()
+            {
+                new PedidoItem(0,null,produto),
+                new PedidoItem(0,null,produto)
+            };
+            pedido.AdicionarItens(pedidoItems,funcionario);
             var regex = new Regex(@"\w+(,)\w+\w+");
-            var match = regex.Match(_pedido.ObterTitulosConcatenados(','));
+            var match = regex.Match(pedido.ObterTitulosConcatenados(','));
             if (!match.Success)
             {
                 Assert.Fail("erro");
             }
+        }
+
+        [TestMethod]
+        public void FazerPedido()
+        {
+            
         }
     }
 }
